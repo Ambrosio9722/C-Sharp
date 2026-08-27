41511d546c4da34a4d9478e76396fd1281670e94
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2026-08-27 17:25:33 -0300

    Hello World!

---
+++ Estudo/Curso/Basico/Basico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
